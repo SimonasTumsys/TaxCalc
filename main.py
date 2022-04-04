841327d48aa50e484d3e05414f563593cab0bad1
SimonasTumsys <simonas.tumsys@gmail.com>
2022-04-04 21:35:09 +0300

    can scan bolt food pdfs, extract dict - date:earn

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 import os
 import json
+import pdfplumber
+from datetime import datetime
 
 
 class CalculatedLayout(GridLayout):
@@ -74,41 +76,67 @@ class CalcWindow(Screen):
 
 class EarnWindow(Screen):
 
-    # def scan_fs():
-    #     with open('pdf_paths.json', 'r') as f:
-    #         pdf_paths = json.load(f)
+    def scan_fs():
+        with open('pdf_paths.json', 'r') as f:
+            pdf_paths = json.load(f)
 
-    #     bf_paths = []
-    #     for root,dirs,files in os.walk("/"):
-    #         bf_paths.extend((os.path.join(root,f) for f in files if f.endswith("Weekly Report.pdf")))
+        bf_paths = pdf_paths['bf']
+        bf_dates = pdf_paths['bf_dates']
+        temp_bf_paths = []
+        temp_w_paths = []
         
-    #     for path in bf_paths:
-    #         pdf = open(path, 'rb')
-    #         pdfReader = PdfFileReader(pdf)
+        for root,dirs,files in os.walk("/"):
+            temp_bf_paths.extend((os.path.join(root,f) for f in files if 'Weekly Report' in f and '.pdf' in f))
+            for temp_bf_path in temp_bf_paths:
+                if temp_bf_path not in bf_paths:
+                    bf_paths.append(temp_bf_path)
 
-    #         print("PDF File name: " + str(pdfReader.getDocumentInfo().title))
-
-    #         bf_obj = {path:"text"}
-
-    #         numOfPages = pdfReader.getNumPages()
-
-    #         for i in range(0, numOfPages):
-    #             print("Page Number: " + str(i))
-    #             print("- - - - - - - - - - - - - - - - - - - -")
-    #             pageObj = pdfReader.getPage(i)
-    #             print(pageObj.extractText())
-    #             print("- - - - - - - - - - - - - - - - - - - -")
-    #         # close the PDF file object
-    #         pdf.close()
-            
-    # scan_fs()
-            
         
-    
-    
-    # def handle_pdf():
-    #     for path in bf_paths:
-    pass
+        pdf_paths['bf'] = bf_paths
+        pdf_paths['bf_dates'] = bf_dates
+
+        with open('pdf_paths.json', 'w') as f:
+            json.dump(pdf_paths, f, indent=2)
+
+    def handle_pdf():
+        ed_dict = {}
+        with open('pdf_paths.json', 'r') as f:
+            pdf_paths = json.load(f)
+## Handling Bolt Food pdfs - extracting date and earnings:
+        for path in pdf_paths['bf']:
+            fixed_path = path.replace('\\', '/')
+            with pdfplumber.open(fixed_path) as temp:
+                pages = []
+                for page in temp.pages:
+                    pages.append(page.extract_text())
+                    pdf_text = ''.join(pages)
+
+            date_start_index = pdf_text.find('Ataskaita už laikotarpį: ') + 25
+            date_end_index = pdf_text.find('Ataskaita už laikotarpį: ') + 48
+            date = pdf_text[date_start_index:date_end_index]
+
+            earn_start_index = pdf_text.find('Savaitinis uždarbis') + 20
+            earn_end_index = pdf_text.find('Savaitinis uždarbis') + 26
+
+            earnings_no_cash = float(pdf_text[earn_start_index:earn_end_index])
+            try:
+                cash_start_index = pdf_text.rfind('Grynieji pinigai iš kliento') + 28
+                cash_end_index = pdf_text.find('Savaitinis uždarbis') - 3
+
+                cash_raw = pdf_text[cash_start_index:cash_end_index]
+                cash_cutoff = cash_raw.find('0.00') + 6
+                cash = float(cash_raw[cash_cutoff:len(cash_raw)])
+            except ValueError:
+                cash = 0.0
+
+            earnings = earnings_no_cash + cash
+
+            ed_dict[date] = earnings
+        return ed_dict
+
+    print(handle_pdf())
+
+
 
 
 class StatWindow(Screen):
