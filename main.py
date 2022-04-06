71df9551a79ee7644f2ead8935f772c56f36afdc
SimonasTumsys <simonas.tumsys@gmail.com>
2022-04-06 15:43:53 +0300

    finished scanning and pdf handling functions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from kivy.uix.gridlayout import GridLayout
 import os
 import json
 import pdfplumber
-from datetime import date, datetime
+import datetime
 import sqlite3
 
 
@@ -104,7 +104,7 @@ class EarnWindow(Screen):
                 with pdfplumber.open(fixed_temp_path) as temp_pdf:
                     pdf_text = temp_pdf.pages[0].extract_text()
     ##Validation to avoid including duplicate PDFs into json   
-                if pdf_text.find('Bolt Operations') != -1:
+                if pdf_text.find('Bolt Operations') != -1 and pdf_text.find('Papildoma pristatymo kaina') == -1:
                     temp_meta = temp_pdf.metadata
                     if temp_path not in bf_paths and temp_meta not in meta_list:
                         bf_paths.append(temp_path)
@@ -130,10 +130,10 @@ class EarnWindow(Screen):
         with open('pdf_paths.json', 'r') as f:
             pdf_paths = json.load(f)
         ## Handling Bolt Food pdfs:
-        for path in pdf_paths['bf']:
+        for bf_path in pdf_paths['bf']:
             try:
-                fixed_path = path.replace('\\', '/')
-                with pdfplumber.open(fixed_path) as temp:
+                fixed_bf_path = bf_path.replace('\\', '/')
+                with pdfplumber.open(fixed_bf_path) as temp:
                     pdf_meta = temp.metadata
                     pages = []
                     for page in temp.pages:
@@ -143,6 +143,8 @@ class EarnWindow(Screen):
                 date_start_index = pdf_text.find('Ataskaita už laikotarpį: ') + 25
                 date_end_index = pdf_text.find('Ataskaita už laikotarpį: ') + 48
                 date = pdf_text[date_start_index:date_end_index]
+                start_date_string = date[0:10]
+                end_date_string = date[13:23]
 
                 earn_start_index = pdf_text.find('Savaitinis uždarbis') + 20
                 earn_end_index = pdf_text.find('Savaitinis uždarbis') + 26
@@ -150,7 +152,7 @@ class EarnWindow(Screen):
                 earnings_no_cash = float(pdf_text[earn_start_index:earn_end_index])
                 try:
                     cash_start_index = pdf_text.rfind('Grynieji pinigai iš kliento') + 28
-                    cash_end_index = pdf_text.find('Savaitinis uždarbis') - 3
+                    cash_end_index = pdf_text.rfind('Grynieji pinigai iš kliento') + 47
 
                     cash_raw = pdf_text[cash_start_index:cash_end_index]
                     cash_cutoff = cash_raw.find('0.00') + 6
@@ -159,10 +161,6 @@ class EarnWindow(Screen):
                     cash = 0.0
 
                 earnings = earnings_no_cash + cash
-
-                start_date_string = date[0:10]
-                end_date_string = date[13:23]
-
                 meta_string = pdf_meta['CreationDate']
 
                 c.execute('INSERT INTO dated_earnings VALUES (?, ?, ?, ?)',
@@ -172,11 +170,50 @@ class EarnWindow(Screen):
                 continue
 
             conn.commit()
+
+        ## Handling Wolt pdfs:
+        for w_path in pdf_paths['wolt']:
+            try:
+                fixed_w_path = w_path.replace('\\', '/')
+                with pdfplumber.open(fixed_w_path) as w_temp:
+                    w_pdf_meta = w_temp.metadata
+                    w_pages = []
+                    for w_page in w_temp.pages:
+                        w_pages.append(w_page.extract_text())
+                        w_pdf_text = ''.join(w_pages)
+
+                w_date_start_index = w_pdf_text.find('Periodas: ') + 10
+                w_date_end_index = w_pdf_text.find('Periodas: ') + 31
+                w_date = w_pdf_text[w_date_start_index:w_date_end_index]
+
+                w_start_date = w_date[0:10].replace('.', '-')
+                w_start_date_string = str(datetime.datetime.strptime(w_start_date,
+                    '%d-%m-%Y').strftime('%Y-%m-%d'))
+
+                w_end_date = w_date[11:21].replace('.', '-')
+                w_end_date_string = str(datetime.datetime.strptime(w_end_date,
+                    '%d-%m-%Y').strftime('%Y-%m-%d'))
+
+                w_earn_start_index = w_pdf_text.find('Suma (EUR)') + 11
+                w_earn_end_index = w_pdf_text.find('Suma (EUR)') + 17
+                w_earnings = float(w_pdf_text[w_earn_start_index:w_earn_end_index])
+
+                w_meta_string = w_pdf_meta['CreationDate']
+
+                c.execute('INSERT INTO dated_earnings VALUES (?, ?, ?, ?)',
+                    (w_start_date_string, w_end_date_string, w_earnings, w_meta_string))
+
+            except sqlite3.IntegrityError:
+                continue
+
+            conn.commit() 
         conn.close()
 
-    def create_db():
+
+    def reset_db():
         conn = sqlite3.connect('pdf_data.db')
         c = conn.cursor()
+        c.execute('''DROP TABLE IF EXISTS dated_earnings''')
         c.execute("""CREATE TABLE IF NOT EXISTS dated_earnings (
             start_date text,
             end_date text,
@@ -186,13 +223,9 @@ class EarnWindow(Screen):
         conn.commit()
         conn.close()
 
-    def clear_db():
-        conn = sqlite3.connect('pdf_data.db')
-        c = conn.cursor()
-        c.execute('''DROP TABLE IF EXISTS dated_earnings''')
-        conn.commit()
-        conn.close()
-
+    reset_db()
+    scan_fs()
+    handle_pdf()
 
 
 class StatWindow(Screen):
